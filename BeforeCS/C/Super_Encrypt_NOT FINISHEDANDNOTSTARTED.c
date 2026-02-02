319f0149405933c074fd13ee410ae9cf2d47575a
ThinhCaoCow <ThinhCaoAn@gmail.com>
2026-02-03 04:29:32 +1100

    Was trying to do cryptography practice but the processes is pretty complex so ill do a more simple project in C++ instead and ill come back to this when im starting my CS courses in UNSW

---
+++ BeforeCS/C/Super_Encrypt_NOT FINISHEDANDNOTSTARTED.c
@@ -0,0 +1,16 @@
+#include <stdio.h> // includes standard input and output functions such as print and scan
+#include <stdlib.h> // important for variable/memory control with commands such as malloc or abs for memory allocation
+#include <string.h> // string data manipulation such as strcpy and strlen
+#include <math.h> // complex math functions such as sin and sqrt
+#include <time.h> // time & date functionalitys
+#include <assert.h> // data diagnostics through assert
+//Basic C libaries that are included  
+
+// day somthing ( i stop tracking since i was procrastinating lol)
+// for the c project, i want to do something cybersecruity related, which being a encrypt and decrypt
+// program. I wanted to do this as i wanted to see the perspective of a randsomware attack from the attackers side
+// from developing a crypt, implementing it to a file, thne decrypting it with a key.
+// With research, there are practical ways of doing this, such as openssl, but i would like to only use
+// the libaries that come with gcc/gnu to develop the ciper as so forth.
+// I will be using a xor stream cipher
+// OK IM COMING BACK TO THIS WHEN I ACTUALLY THINK THIS THROUGH.
